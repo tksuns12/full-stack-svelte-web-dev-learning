2223dbe16a5bcb43579344f9a69651690f58a792
Hyungil Kang(강현길) <tksuns@gmail.com>
2022-11-18 06:49:20 +0000

    change default port into 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import type { UserConfig } from 'vite';
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	server: {
+		port: 3000,
 		hmr: {
 			clientPort: process.env.HMR_HOST ? 443 : 3000,
 			host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
