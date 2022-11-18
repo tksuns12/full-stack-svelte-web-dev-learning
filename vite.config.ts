2d7772b152e2844c3dd794001ee4b64a2405da09
Hyungil Kang(강현길) <tksuns@gmail.com>
2022-11-18 06:37:14 +0000

    move vite config vars to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			clientPort: process.env.HMR_HOST ? 443 : 3000,
+			host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+		}
+	}
 };
 
 export default config;
